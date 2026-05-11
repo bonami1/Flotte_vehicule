01666c7d749e2b42078f6081282d2e4f8bde7859
Nirmala <nirmalabonami@gmail.com>
2026-05-12 00:23:14 +0200

    Développement de la classe VehiculeLourd

---
+++ Flotte_vehicule/src/main/java/fr/flotte/model/VehiculeLourd.java
@@ -0,0 +1,59 @@
+package fr.flotte.model;
+
+public class VehiculeLourd extends Vehicule implements Assignable, Maintenable {
+    private double capaciteCharge;
+    private int nombreEssieux;
+
+    public VehiculeLourd(String immatriculation, String marque, String modele, double kilometrage, double capaciteCharge, int nombreEssieux) {
+        super(immatriculation, marque, modele, kilometrage);
+        this.capaciteCharge = capaciteCharge;
+        this.nombreEssieux = nombreEssieux;
+    }
+
+    public double getCapaciteCharge() { return capaciteCharge; }
+    public int getNombreEssieux() { return nombreEssieux; }
+    public void setCapaciteCharge(double capaciteCharge) { this.capaciteCharge = capaciteCharge; }
+    public void setNombreEssieux(int nombreEssieux) {this.nombreEssieux = nombreEssieux;}
+
+    /*@Override
+    public void assigner(Mission mission) {
+        ajouterMission(mission);
+        setEtat(EtatVehicule.UTILISE);
+    }*/
+
+    @Override
+    public boolean estDisponible() {
+        return getEtat() == EtatVehicule.DISPONIBLE;
+    }
+
+    @Override
+    public void effectuerMaintenance() {
+        setEtat(EtatVehicule.EN_MAINTENANCE);
+    }
+
+    /*@Override
+    public void signalerIncident(Incident incident) {
+        ajouterIncident(incident);
+        setEtat(EtatVehicule.EN_MAINTENANCE);
+    }*/
+
+    public boolean verifierCharge(double poids) {
+         return poids <= capaciteCharge;
+    }
+
+    public double calculerUsure() {
+        double usureKilometrage = getKilometrage() / 10000;
+        //double usureIncidents = historiqueIncidents.size() * 5;
+        double usureCharge = capaciteCharge / 1000;
+
+        return usureKilometrage + /*usureIncidents*/ + usureCharge;
+    }
+
+    @Override
+    public String toString() {
+        return "VehiculeLourd{" +
+                "capaciteCharge=" + capaciteCharge +
+                ", nombreEssieux=" + nombreEssieux +
+                '}';
+    }
+}
